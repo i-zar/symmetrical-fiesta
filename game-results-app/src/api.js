3fefbe4554593a4d62668a75fc902d8d017e117c
Ivan <ivan.zharkevich@gmail.com>
2023-07-30 22:23:42 +0200

    Add app.py location to procfile

--- game-results-app/src/api.js
+++ game-results-app/src/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'http://game-results-app-env.eba-suvm3xvy.eu-central-1.elasticbeanstalk.com/api',
+  //http://game-results-app-env.eba-suvm3xvy.eu-central-1.elasticbeanstalk.com/api
+  //http://localhost:5000/api
 });
 
 export default api;
