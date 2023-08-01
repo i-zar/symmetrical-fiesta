27331f72acd540761755bc763cff741413655b20
Ivan <ivan.zharkevich@gmail.com>
2023-08-01 18:57:24 +0200

    revert baseURL to localhost

--- game-results-app/src/api.js
+++ game-results-app/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://game-results-app.eu-central-1.elasticbeanstalk.com/api/',
+  baseURL: 'http://localhost:5000/api',
   //http://game-results-app.eu-central-1.elasticbeanstalk.com/api/
   //http://localhost:5000/api
 });
